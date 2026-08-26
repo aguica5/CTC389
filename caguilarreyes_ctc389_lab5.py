38257a417c2c33162dafe7d94f75148e35b1a350
aguica5 <aguica@fusd.net>
2026-08-26 23:59:22 +0000

    lab 5

---
+++ caguilarreyes_ctc389_lab5.py
@@ -0,0 +1,40 @@
+#Carmen Aguilar-Reyes
+#Lab
+
+word = "No Loop"
+
+n1 = 30
+n2 = 32
+n3 = 34
+n4 = 36
+n5 = 38
+n6 = 40 
+n7 = 42
+n8 = 44
+n9 = 46
+n10 = 48 
+
+print(word)
+print(n1)
+print(n2)
+print(n3)
+print(n4)
+print(n5)
+print(n6)
+print(n7)
+print(n8)
+print(n9)
+print(n10)
+
+nums = ["With Loop", 30, 32, 34, 36, 38, 40, 42, 44, 46, 48]
+
+for i in nums: 
+    print (i)
+
+words = ["cherries", "apricot", "zucchinis", "bell pepper", "banana"]
+print (words[0])
+print (words[1])
+print (words[2])
+print (words[3])
+print (words[4])
+
