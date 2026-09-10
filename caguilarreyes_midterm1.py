c8a1ff2b5ef5a276149778a46d1c1111cd3c50ba
aguica5 <aguica@fusd.net>
2026-09-10 00:22:53 +0000

    midterm1

---
+++ caguilarreyes_midterm1.py
@@ -0,0 +1,14 @@
+#Carmen Aguilar-Reyes
+#Midterm Python Program #1
+
+
+name = input("Hey, what's your name? ")
+
+yournumber= int(input(print("Enter a number: ")))
+
+addnumber= int(input(print("How much do you want to add to your original number?: ")))
+
+total = (yournumber + addnumber) 
+
+print("Hey", name, "you entered", yournumber, "and added", addnumber, "to get a total of", total)
+
