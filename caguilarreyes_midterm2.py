7be23912715e719f145484383089d7943e78a075
aguica5 <aguica@fusd.net>
2026-09-10 00:40:34 +0000

    midterm2

---
+++ caguilarreyes_midterm2.py
@@ -0,0 +1,13 @@
+#Carmen Aguilar-Reyes
+#Midterm Python Program #2
+
+def triarea(): 
+    area = (base*height)/2
+    print("The area of the triangle is: ", area)
+
+base= float(input("enter the base of your triangle: "))
+height = float (input("enter the height of your triangle: "))
+
+triarea()
+
+
